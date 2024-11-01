1c2bed360c20febf1b68ce03246ce75c49d0e578
Rikivos <rizkypenpenz@gmail.com>
2024-11-02 05:25:04 +0700

    penambahan navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 // src/App.jsx
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import LandingPage from './landing/LandingPage';
+import Footer from './components/Footer';
+
+
+
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <LandingPage />
+      <Footer />
     </div>
   );
 }
