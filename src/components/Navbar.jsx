8106936df8277f77ddb409dc65859eb388f9261c
Rikivos <rizkypenpenz@gmail.com>
2024-11-11 02:45:52 +0700

    Update ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 // src/components/Navbar.jsx
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <nav className="fixed top-0 left-0 w-full flex items-center justify-between py-2 px-6 bg-white border-b shadow-md z-10">
       <div className="flex items-center">
@@ -10,27 +13,42 @@ function Navbar() {
       {/* Apply ml-auto to move the list items to the right */}
       <ul className="flex space-x-8 text-black ml-auto">
         <li>
-          <a href="/" className="hover:text-gray-700">
+          <button
+            onClick={() => navigate("/")}
+            className="hover:text-gray-700 focus:outline-none"
+          >
             Beranda
-          </a>
+          </button>
         </li>
         <li>
-          <a href="/artikel" className="hover:text-gray-700">
+          <button
+            onClick={() => navigate("/artikel")}
+            className="hover:text-gray-700 focus:outline-none"
+          >
             Artikel
-          </a>
+          </button>
         </li>
         <li>
-          <a href="#" className="hover:text-gray-700">
+          <button
+            onClick={() => navigate("/tentang-kami")}
+            className="hover:text-gray-700 focus:outline-none"
+          >
             Tentang Kami
-          </a>
+          </button>
         </li>
         <li>
-          <a href="#" className="hover:text-gray-700">
+          <button
+            onClick={() => navigate("/kontak")}
+            className="hover:text-gray-700 focus:outline-none"
+          >
             Kontak
-          </a>
+          </button>
         </li>
       </ul>
-      <button className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md ml-4">
+      <button
+        onClick={() => navigate("/get-started")}
+        className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md ml-4"
+      >
         Get Started
       </button>
     </nav>
